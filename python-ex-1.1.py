3a85ef379f862580043dfd2ccdb8fcfbbe2942bc
Sude Akınay <nesrinakinay.s@gmail.com>
2022-05-10 12:15:13 +0300

    Update python-ex-1.1.py

--- python-ex-1.1.py
+++ python-ex-1.1.py
@@ -1,87 +1,87 @@
 
-defkullanıcı = 'büyücü'
-defparola = 'kuşlariseviyor'
+defuser = 'magician'
+defpassword = 'lovesbirds'
 while(True):
-    kullanıcı = input('kullanıcı adı:')
-    parola = input('parola: ')
-    if((kullanıcı == defkullanıcı) and (parola == defparola)):
-        print('hoşgeldiniz', kullanıcı)
+    user = input('user name:')
+    password = input('password: ')
+    if((username == defuser) and (password == defpassword)):
+        print('hosgeldiniz', username)
         break
-    elif ((kullanıcı != defkullanıcı) and (defparola == parola)):
-        print('kullanıcı adını yanlış girdiniz')
-    elif((kullanıcı == defkullanıcı) and (parola != defparola)):
-        print('şifrenizi mi unuttunuz?')
-        print('şifrenizi yenilemek ister misiniz?')
-        cevap = input()
-        if(cevap == 'E'):
-            yeniparola = input('yeni parola: ')
-            print('bekleyin....')
-            defparola = yeniparola
-            print('şifreniz güncellenmiştir...')
+    elif ((username != defuser) and (defpassword == password)):
+        print('wrong user name')
+    elif((username == defuser) and (password != defpassword)):
+        print('did you forgot your passord?')
+        print('do you want to update your password?')
+        answer = input()
+        if(answer == 'E'):
+            newpassword = input('new password: ')
+            print('wait....')
+            defparola = newpassword
+            print('your password has been updated...')
     else:
-        print('tekrar deneyiniz....')
+        print('try eagain....')
 ###########################
 
-faktoriyel =1
+factorial =1
 while True:
-    sayı = int(input('negatif olmayan bir sayı girin '))
-    if(sayı <= 0 ):
-        print('negatif olmayan bir sayi girin')
+    number = int(input('enter a positive number '))
+    if(number <= 0 ):
+        print('enter a positive number, please')
     else: 
-        for i in range(1, sayı+1):
+        for i in range(1, number+1):
 
-            faktoriyel *=i
-        print('faktoriyel', faktoriyel)
+            factorial *=i
+        print('factorial', factorial)
         break
 ###########################
 
-def geometri(sekil):
-    if len(sekil) == 3:
-        a = sekil[0]
-        b = sekil[1]
-        c = sekil[2]
+def geometry(shape):
+    if len(shape) == 3:
+        a = shape[0]
+        b = shape[1]
+        c = shape[2]
 
         if (a+b) > c and (a+c) > b and (b+c) > a:
             if (a == b) and (a == c) and (b == c):
-                print('bu bir eşkenar üçgendir....')
+                print('It is an equilateral triangle.....')
             elif (a == b) and (a == c):
-                print('bu bir ikizkenar üçgendir....')
+                print('It is an isosceles triangle.....')
             else:
-                  print('bu bir çeşitkenar üçgendir....')  
+                  print('It is a scalene triangle.....')  
                 
-    elif len(sekil) == 4:
-        x = sekil[0]
-        y = sekil[1]
-        z = sekil[2]
-        k = sekil[3]
+    elif len(shape) == 4:
+        x = shape[0]
+        y = shape[1]
+        z = shape[2]
+        k = shape[3]
 
         if (x == y) and (y == z) and (z == k) and (k == x):
-            print('bu bir karedir')
+            print('It is a square...')
         elif (x == z) and (y == k):
-            print('bu bir dikdörtgendir....')
+            print('It is a rectangle.....')
         else:
-            print('bu normal bir dörtgendir....')
+            print('It is a regular quadrilateral.....')
 
     else:
-        print('herhangi bir şekil değil....')
+        print('It is not any shape.....')
 
 
 while True:
-    eleman_sayisi = int(input('eleman sayısını giriniz: '))
-    if (eleman_sayisi == 3):
+    number_of_elements = int(input('enter the number of elements: '))
+    if (shape == 3):
         a = int(input('a: '))
         b = int(input('b: '))
         c = int(input('c: '))
-        geometri([a, b, c])
+        geometry([a, b, c])
 
-    elif (eleman_sayisi == 4):
+    elif (shape == 4):
         x = int(input('x: '))
         y = int(input('y: '))
         z = int(input('z: '))           
         k = int(input('k: ')) 
-        geometri([x, y, z, k])
+        geometry([x, y, z, k])
 
     else:
-        print('lütfen tekrar giriniz...')
+        print('please re-enter...')
 
 
